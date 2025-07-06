cb0da392de26833495d150e1d5de461081994ea8
DebasishBisoi524 <debaishbisoi524@gmail.com>
2025-07-06 20:54:35 +0530

    commit

---
+++ week2/CalcLibrary/CalcLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CalcLibrary.Tests
+{
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _calculator = null;
+        }
+
+        [Test]
+        [TestCase(1, 2, 3)]
+        [TestCase(5, 7, 12)]
+        [TestCase(-2, 3, 1)]
+        public void Add_ShouldReturnCorrectSum(int a, int b, int expected)
+        {
+            var result = _calculator.Add(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ week2/CalcLibrary/CalcLibrary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcLibrary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcLibrary.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f0797d2-fdb9-4b52-9e7f-a06540347eb5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
