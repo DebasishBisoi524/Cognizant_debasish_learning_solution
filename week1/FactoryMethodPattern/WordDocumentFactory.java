3b4f21f0d364e9a6d0fc3f231b7a48da02457592
DebasishBisoi524 <debaishbisoi524@gmail.com>
2025-07-06 09:55:52 +0530

    saved

--- week1/FactoryMethodPattern/WordDocumentFactory.java
+++ week1/FactoryMethodPattern/WordDocumentFactory.java
@@ -1,7 +1,6 @@
 package week1.FactoryMethodPattern;
 
 public class WordDocumentFactory extends DocumentFactory {
-    @Override
     public Document createDocument() {
         return new WordDocument();
     }
